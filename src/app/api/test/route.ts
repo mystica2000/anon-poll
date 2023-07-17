bf0e0b105a9ebdb02614c462c49145a31963d0d5
mystica2000 <mysticainf@gmail.com>
2023-07-17 21:52:53 +0530

    rename to .ts

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ name: 'Mystica' });
+  return NextResponse.json({ name: 'Mystica Test' });
 }
